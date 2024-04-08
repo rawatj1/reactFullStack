94800d184e36df1a848fbaf3b0d3cf1fd06a6a0c
Jagdish Singh Rawat <jagdish.singhrawat@degruyter.com>
2024-04-08 22:33:22 +0200

    Part2_1 exercise

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,5 @@
-const Header = (props) => {
-    return (
-        <h1>{props.course}</h1>
-    )
-}
-
-const Content = (props) => {
-    const partContent = props.parts.map( part => (
-        <p key = {part.name}>
-            {part.name} {part.exercise}
-        </p>
-    ));
-    console.log(partContent)
-    return (
-        <div>
-            {partContent}
-        </div>
-    )
-}
-const Total = (props) => {
+import Course from "./components/Course.jsx";
+/*const Total = (props) => {
     let total = 0;
     props.parts.forEach(value => {
         total = total + value.exercise
@@ -26,31 +8,37 @@ const Total = (props) => {
     return (
         <p>Number of exercises: {total}</p>
     )
-}
+}*/
 /* eslint-disable no-unused-vars */
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercise: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercise: 7
+    const course = {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                id: 1,
+                name: 'Fundamentals of React',
+                exercise: 10
 
-        },
-        {
-            name: 'State of a component',
-            exercise: 14
-        }
-    ]
+            },
+            {
+                id: 2,
+                name: 'Using props to pass data',
+                exercise: 7
+
+            },
+            {
+                id: 3,
+                name: 'State of a component',
+                exercise: 14
+            }
+
+        ]
+    }
 
     return (
         <div>
-            <Header course={course}/>
-            <Content parts={parts}/>
-            <Total parts={parts}/>
+            <Course course={course} />
         </div>
     )
 }
