437598c9f026adea1b9b62372c1ee5aa6a4b330a
Jagdish Singh Rawat <jagdish.singhrawat@degruyter.com>
2024-04-08 23:40:09 +0200

    Part2_4 exercise completed

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,34 +2,59 @@ import Course from "./components/Course.jsx";
 
 /* eslint-disable no-unused-vars */
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                id: 1,
-                name: 'Fundamentals of React',
-                exercise: 10
+    const courses = [
+        {
+            id: 1,
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    id: 1,
+                    name: 'Fundamentals of React',
+                    exercise: 10
 
-            },
-            {
-                id: 2,
-                name: 'Using props to pass data',
-                exercise: 7
+                },
+                {
+                    id: 2,
+                    name: 'Using props to pass data',
+                    exercise: 7
 
-            },
-            {
-                id: 3,
-                name: 'State of a component',
-                exercise: 14
-            }
+                },
+                {
+                    id: 3,
+                    name: 'State of a component',
+                    exercise: 14
+                },
+                {
+                    id: 4,
+                    name: 'Redux',
+                    exercise: 11
+                }
 
-        ]
-    }
+            ]
+        }, {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercise: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercise: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
     return (
         <div>
-            <Course course={course} />
+            <h1>Web Development curriculum</h1>
+            {courses.map( course =>
+                <Course key={course.id} course={course}/>
+            )}
         </div>
     )
 }
